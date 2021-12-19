a23654d08ca0d800d3572211f2c02b80c59ce545
Ayrton <54418646+zinhunter@users.noreply.github.com>
2021-12-19 16:42:21 -0600

    Revert "Add guild_exists function and defaults for structures"

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -23,23 +23,9 @@ impl Default for GuildsPlatform {
     }
   }
 
-/// Default trait for Guild
-impl Default for Guild {
-    fn default() -> Self {
-        new_guild()
-    }
-}
-
-/// Default trait for UpgradableGuild
-impl Default for UpgradableGuild {
-    fn default() -> Self {
-        UpgradableGuild::CurrentVersion(new_guild())
-    }
-}
-
 
 //Implementation for upgradable contract
-#[derive(Serialize, Deserialize, BorshSerialize, BorshDeserialize, Debug)]
+#[derive(Serialize, Deserialize, BorshSerialize, BorshDeserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub enum UpgradableGuild {
     CurrentVersion(Guild),
@@ -53,7 +39,7 @@ impl From<UpgradableGuild> for Guild {
     }
 }
 
-#[derive(Serialize, Deserialize, BorshSerialize, BorshDeserialize, Debug)]
+#[derive(Serialize, Deserialize, BorshSerialize, BorshDeserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct Guild {
     pub slug: String,
@@ -154,30 +140,8 @@ impl GuildsPlatform {
         guild.members.len()
     }
 
-    /// Returns true if the guild exists
-    pub fn guild_exists(&self, slug: &String) ->bool {
-        match self.guilds.get(&slug) {
-            Some(value) => {
-                let log_message = format!("Guild exists {:?}", value);
-                env::log(log_message.as_bytes());
-                true
-            },
-            None => false
-        }
-    }
-
-    /// Returns true if the current account is member of the Guild
     pub fn check_if_member(&self, slug: String) -> bool {
-        let guild: Guild;
-        match self.guild_exists(&slug) {
-            //true => let guild: Guild = self.guilds.get(&slug).expect("Guild does not exist").into();
-            true => guild = self.guilds.get(&slug).unwrap_or_default().into(),
-            false => {
-                let msg = format!("Guild {} does not exist", slug);
-                env::log(msg.as_bytes());
-                return false;
-            }
-        }
+        let guild: Guild = self.guilds.get(&slug).unwrap().into();
 
         match guild.members.get(&env::predecessor_account_id()) {
             Some(_member) => true,
@@ -185,28 +149,3 @@ impl GuildsPlatform {
         }
     }
 }
-
-/// Returns an empty Guild
-/// TODO: Create a new "Constructor" trait?
-pub fn new_guild() -> Guild {
-    env::log(format!("Emplty guild created ").as_bytes());
-    Guild {
-        slug: String::from(""),
-        title: String::from(""),
-        oneliner: String::from(""),
-        website: String::from(""),
-        app: String::from(""),
-        whitepaper: String::from(""),
-        twitter: String::from(""),
-        telegram: String::from(""),
-        discord: String::from(""),
-        medium: String::from(""),
-        github: String::from(""),
-        ticker: String::from(""),
-        logo: String::from(""),
-        contract_str: String::from(""),
-        youtube: String::from(""),
-        members: HashSet::new(),
-    }
-}
-
