0c889f1a162bf42d9778937661e4aba2bd7055e5
JoyceLozanoOP <joycseleneanaid@gmail.com>
2021-12-19 16:14:01 -0600

    Add image while cards are loading

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -20,9 +20,15 @@ import {
   Container, 
   Row
 } from "reactstrap";
+import { useState } from "react";
 
 export default function MainPage({guilds}) {
-  
+  const [loaded, setLoaded] = useState(true);
+
+  function onLoad() {
+    setLoaded(false);
+  }
+
   return (
     <div className="section section-examples" data-background-color="black">
       <img
@@ -33,7 +39,12 @@ export default function MainPage({guilds}) {
       <div className="space-50" />
       <Container className="text-center">
         <h2 className="title">Find a Guild</h2>
-        <Row>
+        <img 
+          style={{display: loaded ? 'block': 'none'}}
+          src={require("assets/img/loading-23.gif").default}
+          alt={"Loading..."} 
+        /> 
+        <Row onLoad={onLoad}>
           {
             guilds.map(guild => {
               return (
